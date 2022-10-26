b7886a78b934dc33041e52c0f86b574b0bb71397
Majedul Islam Leon <majedulleon@gmail.com>
2022-10-26 19:41:40 +0600

    First Input

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let newTask = document.querySelector('#new-task');
+let form = document.querySelector('form');
+let todoUL = document.querySelector('#items');
+let completeUL = document.querySelector('.complete-list ul');
